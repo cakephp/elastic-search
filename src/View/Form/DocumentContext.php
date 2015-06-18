7eafcfd91e615e4f50fc63a3e39e47ba037fe84c
Mark Story <mark@mark-story.com>
2015-06-17 22:37:22 -0400

    Use the new mapping schema in the document context class.
    Less code in the DocumentContext class.

--- src/View/Form/DocumentContext.php
+++ src/View/Form/DocumentContext.php
@@ -305,8 +305,8 @@ class DocumentContext implements ContextInterface
      */
     public function fieldNames()
     {
-        $fields = $this->_context['type']->schema();
-        return array_keys($fields);
+        $schema = $this->_context['type']->schema();
+        return $schema->fields();
     }
 
     /**
@@ -315,22 +315,7 @@ class DocumentContext implements ContextInterface
     public function type($field)
     {
         $schema = $this->_context['type']->schema();
-        if (isset($schema[$field]['type'])) {
-            return $schema[$field]['type'];
-        }
-        if (strpos($field, '.') !== false) {
-            $parts = explode('.', $field);
-            $pointer = $schema;
-            foreach ($parts as $part) {
-                if (isset($pointer[$part]['type']) && $pointer[$part]['type'] !== 'nested') {
-                    return $pointer[$part]['type'];
-                }
-                if (isset($pointer[$part]['properties'])) {
-                    $pointer = $pointer[$part]['properties'];
-                }
-            }
-        }
-        return null;
+        return $schema->fieldType($field);
     }
 
     /**
