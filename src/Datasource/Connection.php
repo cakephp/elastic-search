942f9d26d14804062263a966ea2836a9a648f51d
Damiano Porta <damianoporta@gmail.com>
2016-06-09 10:09:58 +0200

    Update Connection.php

--- src/Datasource/Connection.php
+++ src/Datasource/Connection.php
@@ -201,6 +201,11 @@ class Connection extends Client implements ConnectionInterface
         $data = json_encode($logData, JSON_PRETTY_PRINT);
         $loggedQuery = new LoggedQuery();
         $loggedQuery->query = $data;
-        $this->_logger->log($loggedQuery);
+        
+        if ($this->_logger instanceof \Psr\Log\LoggerInterface) {
+            $this->_logger->log('debug', $loggedQuery);
+        } else {
+            $this->_logger->log($loggedQuery);
+        }
     }
 }
